c4c5cc19b2c7d5e62ad698aa05ff7c08122e4e18
Ayush Prashar <ayush.prashar@gmail.com>
2019-02-04 12:53:11 +0530

    Question 2 Java refactored

--- java/Question2.java
+++ java/Question2.java
@@ -7,30 +7,22 @@ public class Question2 {
 	}
 
 	private static int getEvenFibonacciSum(int firstFibonacciTerm, int secondFibonacciTerm,int sumOfTerms) {
+		
 		int newFibonacciTerm = firstFibonacciTerm + secondFibonacciTerm;
-		System.out.println("First term is " + firstFibonacciTerm);
-		System.out.println("Second term is " + secondFibonacciTerm);
-		System.out.println("The newFibonacciTerm out of " + firstFibonacciTerm + 
-					" " + secondFibonacciTerm + " is " + newFibonacciTerm);
+		
 		if( newFibonacciTerm > PAR_VALUE) {
-			System.out.println("Value greater than PAR_VALUE is " + sumOfTerms);
 			return sumOfTerms;
 		}
 		else {
+		
 			if (newFibonacciTerm % 2 == 0) {
 				int temp = sumOfTerms + newFibonacciTerm;
-				System.out.println( newFibonacciTerm + " is even and hence the new sum is " + temp);
 				return getEvenFibonacciSum(secondFibonacciTerm, newFibonacciTerm, temp);
 			}
 			else {
-				System.out.println( newFibonacciTerm + " is odd and hence the sumOfTerms still is " + sumOfTerms );
-
 				return getEvenFibonacciSum(secondFibonacciTerm, newFibonacciTerm, sumOfTerms);
 			}
 		}
 	}
 
-/*	private static int getNextFibonacciTerm(int previousTerm, int previousToPerviousTerm) {
-		return previousTerm + previousToPerviousTerm;
-	}*/
 }
